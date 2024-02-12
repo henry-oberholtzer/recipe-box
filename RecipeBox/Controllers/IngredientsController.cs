c4aa5d5ff315d824037fbdfbe6aa4754798dfb87
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-02-12 14:33:12 -0800

    ingredient form added, ingredient create delete and edit

--- RecipeBox/Controllers/IngredientsController.cs
+++ RecipeBox/Controllers/IngredientsController.cs
@@ -11,6 +11,14 @@ public class IngredientsController : Controller
   {
     _db = db;
   }
+
+  private static Dictionary<string, object> IngredientFormModel(Ingredient ingredient, string action)
+  {
+    return new Dictionary<string, object> {
+      {"Ingredient", ingredient},
+      {"Action", action}
+    };
+  }
   public ActionResult Index()
   {
     List<Ingredient> model = _db.Ingredients
@@ -19,5 +27,21 @@ public class IngredientsController : Controller
     return View(model);
   }
 
+  public ActionResult Create()
+  {
+    return View(IngredientFormModel(new Ingredient(), "Create"));
+  }
+
+  [HttpPost]
+  public ActionResult Create(Ingredient ingredient)
+  {
+    if(!ModelState.IsValid)
+    {
+      return View(IngredientFormModel(ingredient, "Create"));
+    }
+    _db.Ingredients.Add(ingredient);
+    _db.SaveChanges();
+    return RedirectToAction("Details", new { id = ingredient.IngredientId});
+  }
 
 }
--- RecipeBox/Models/Ingredient.cs
+++ RecipeBox/Models/Ingredient.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RecipeBox.Models;
 
 public class Ingredient
 {
   public int IngredientId { get; set; }
+
+  [Required]
+  [Display(Name = "Ingredient Name")]
+  [StringLength(255, ErrorMessage = "The ingredient may not be longer than 255 characters")]
   public string Name { get; set; }
   public List<IngredientRecipe> IngredientRecipes { get; set; }
 }
